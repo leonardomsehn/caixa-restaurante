632165df3aaa2d37f21e58f3b42a9750dc6dee94
Leonardo Sehn <leonardomsehn@gmail.com>
2024-10-04 21:50:22 -0300

    commit inicial

---
+++ src/componentes/ItemForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './css/ItemForm.css';
+
+export default function ItemForm({ image, title, content, price, quantity, onIncrease, onDecrease }) {
+  return (
+    <div className="card">
+      <img src={image} alt={title} />
+      <h2>{title}</h2>
+      <p>{content}</p>
+      <p>Preço: R${price}</p>
+
+    </div>
+  );
+}
---
+++ src/componentes/TotalBill.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TotalBill() {
+  return (
+    <div>
+
+    </div>
+  )
+}
